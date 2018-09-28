cb1ffced7c3566870653c522abab6b5a9ea857b8
pikaqiu <1047984367@qq.com>
2018-09-29 01:13:14 +0800

    第一次提交

--- src/main/java/com/pikaqiu/rabbitmq/stream/Barista.java
+++ src/main/java/com/pikaqiu/rabbitmq/stream/Barista.java
@@ -8,8 +8,8 @@ import org.springframework.messaging.SubscribableChannel;
  * <B>概要说明：</B><BR>
  * 这里的Barista接口是定义来作为后面类的参数，这一接口定义来通道类型和通道名称。
  * 通道名称是作为配置用，通道类型则决定了app会使用这一通道进行发送消息还是从中接收消息。
- * @author ashen（Alienware）
- * @since 2016年7月22日
+ * @author xiaoye
+ * @since 2018年7月22日
  */
 
 public interface Barista {
