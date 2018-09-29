a7b86a720289e6a52a0b618c144e80f7631790e0
lixinyi <xinyi.li2@pactera.com>
2018-09-29 17:21:28 +0800

    添加注释

--- src/main/java/com/pikaqiu/rabbitmq/stream/RabbitmqReceiver.java
+++ src/main/java/com/pikaqiu/rabbitmq/stream/RabbitmqReceiver.java
@@ -14,11 +14,11 @@ import com.rabbitmq.client.Channel;
 public class RabbitmqReceiver {  
 
     @StreamListener(Barista.INPUT_CHANNEL)  
-    public void receiver(Message message) throws Exception {  
+    public void receiver(Message message) throws Exception {
 		Channel channel = (com.rabbitmq.client.Channel) message.getHeaders().get(AmqpHeaders.CHANNEL);
 		Long deliveryTag = (Long) message.getHeaders().get(AmqpHeaders.DELIVERY_TAG);
     	System.out.println("Input Stream 1 接受数据：" + message);
     	System.out.println("消费完毕------------");
     	channel.basicAck(deliveryTag, false);
-    }  
+    }
 }  
