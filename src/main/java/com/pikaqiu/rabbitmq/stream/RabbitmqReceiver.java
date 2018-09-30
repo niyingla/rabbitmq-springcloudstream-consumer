4b9e7dccb962feb056d6ee8cda7bd380901880c6
lixinyi <xinyi.li2@pactera.com>
2018-09-30 11:11:07 +0800

    添加注释

--- src/main/java/com/pikaqiu/rabbitmq/stream/RabbitmqReceiver.java
+++ src/main/java/com/pikaqiu/rabbitmq/stream/RabbitmqReceiver.java
@@ -1,12 +1,11 @@
 package com.pikaqiu.rabbitmq.stream;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
-
-import com.rabbitmq.client.Channel;
 
 //stream这个注解必须
 @EnableBinding(Barista.class)
@@ -14,7 +13,7 @@ import com.rabbitmq.client.Channel;
 public class RabbitmqReceiver {  
 
 	//监听这个
-    @StreamListener(Barista.INPUT_CHANNEL)  
+    @StreamListener(Barista.INPUT_CHANNEL)
     public void receiver(Message message) throws Exception {
 		Channel channel = (com.rabbitmq.client.Channel) message.getHeaders().get(AmqpHeaders.CHANNEL);
 		Long deliveryTag = (Long) message.getHeaders().get(AmqpHeaders.DELIVERY_TAG);
